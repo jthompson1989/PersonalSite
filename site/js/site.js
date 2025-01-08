fe2a1302078c726fa9c26a34405c85fdc64b0b3c
Joseph Thompson <zombiechan51@gmail.com>
2025-01-07 18:26:08 -0600

    Fixed issue with ShowDiv function

--- site/js/site.js
+++ site/js/site.js
@@ -2,19 +2,24 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  slideIndex += n;
+  if (slideIndex > document.getElementsByClassName("mySlides").length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = document.getElementsByClassName("mySlides").length}
+  showDivs(slideIndex);
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  else if (n < 1) {slideIndex = x.length}
-  else {slideIndex = n;}
+  if (n > x.length) {n = 1}
+  if (n < 1) {n = x.length}
+  slideIndex = n;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  if (slideIndex > 0 && slideIndex <= x.length) {
+    x[slideIndex-1].style.display = "block";
+  }
 }
 
 function loadSite(){
@@ -27,7 +32,7 @@ function renderYears(){
     renderYear("msSqlYear", new Date(2010, 0, 1));
     renderYear("postgresYear", new Date(2023, 0, 1));
     renderYear("webYear", new Date(2008, 0, 1));
-    renderYear("angularYear", new Date(2014, 0, 1));
+    renderYear("angularYear", new Date(2016, 0, 1));
     renderYear("javaYear", new Date(2010, 0, 1));
 }
 
